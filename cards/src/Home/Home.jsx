9d011f4cde9e08e5eae2b27acdf2f66ec16874e1
Abhinandan Mishra <abhinandanmishra360@gmail.com>
2022-05-30 00:47:10 +0530

    Create home card

--- cards/src/Home/Home.jsx
+++ cards/src/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import HomeCard from './HomeCard/HomeCard'
+import './home.css'
 const Home = () => {
   return (
-    <div className='cards'>
-        <HomeCard />
+    <div className='main-cards'>
+        <HomeCard cardTitle="Card One" cardNumber={1} cardDetails="This is first card" cardLink="card1"/>
+        <HomeCard cardTitle="Card Two" cardNumber={2} cardDetails="This is second card" cardLink="card1"/>
     </div>
   )
 }
--- cards/src/Home/HomeCard/HomeCard.jsx
+++ cards/src/Home/HomeCard/HomeCard.jsx
@@ -2,25 +2,22 @@ import React from 'react'
 import { useNavigate } from "react-router-dom";
 import "./homeCard.css"
 
-const HomeCard = () => {
+const HomeCard = ({cardNumber,cardTitle,cardDetails,cardLink}) => {
     const navigate = useNavigate();
 	const goToCard = (card) => {
 		navigate(`/${card}`);
 	};
 	return (
-		<div className="home-card-container" onClick={() => goToCard("card1")}>
+		<div className="home-card-container" >
 			<div className="home-card">
 				<div className="home-card-box">
 					<div className="home-card-content">
-						<h2>01</h2>
-						<h3>Card One</h3>
+						<h2>{cardNumber<10?"0"+cardNumber:cardNumber}</h2>
+						<h3>{cardTitle}</h3>
 						<p>
-							Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
-							rem esse aperiam eum placeat sed debitis unde sit cupiditate
-							maxime hic sapiente iure enim pariatur reiciendis beatae veritatis
-							neque repellendus dolorum, consectetur harum totam modi! Fugit
-							sint aliquam amet eaque!
+							{cardDetails}
 						</p>
+						<span onClick={() => goToCard(cardLink)} >See Card Design</span>
 					</div>
 				</div>
 			</div>
